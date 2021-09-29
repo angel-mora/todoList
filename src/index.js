8ce7533c0928aa77b2e0d493f4b2683025c62917
AngelMoraDev <angelmomaa@gmail.com>
2021-09-29 12:12:31 -0500

    set src basics for html and js

---
+++ src/index.js
@@ -0,0 +1,11 @@
+const exampleTasks = [
+  {'index': 1, 'description': 'This is a heavy task.', 'completed': false},
+  {'index': 2, 'description': 'This is a heavy task.', 'completed': false},
+  {'index': 3, 'description': 'This is a heavy task.', 'completed': false}
+]
+
+const tasksIterator = (exampleTasks) => {
+  exampleTasks.forEach(task => {
+    // return li {task}
+  });
+}
